f09d4b716b05c5a4aa4c208f18ec1163a76c878f
chirag <chiraggujar21@gmail.com>
2025-11-09 16:46:44 +0000

    Added Dependencies {requirements.txt}

--- main.py
+++ main.py
@@ -7,10 +7,12 @@ recogniser = sr.Recognizer()
 engine = pyttsx3.init()
 rate = engine.getProperty('rate')
 engine.setProperty('rate', rate - 50) 
+
+
 def speak(text):
     engine.say(text)
     engine.runAndWait()
     engine.stop()
 
-speak("hello sir")
+speak("Hello sir, ")
 speak("Initializing Jarvis")
